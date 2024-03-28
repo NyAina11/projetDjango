b4abe2fbc1f8ea6b4a23aaabd08eb647585a3654
NyAina11 <rakotonirinanyaina11@gmail.com>
2024-03-28 08:50:04 +0000

    migration

--- page/models.py
+++ page/models.py
@@ -1,4 +1,6 @@
 from django.db import models
 
 class Game(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=64, unique=True)
+    studio = models.CharField(max_length=64, unique=True)
+    release_date = models.DateField("date released")
--- page/urls.py
+++ page/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatters = [
+urlpatterns = [
     path('', views.index, name='index')
 ]
