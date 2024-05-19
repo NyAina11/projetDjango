68372018263078ce4b9cda675dac1804f261db10
NyAina11 <raktnirinanyaina11@gmail.com>
2024-05-19 11:45:33 +0000

    corr

--- page/models.py
+++ page/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Game(models.Model):
     name = models.CharField(max_length=64, unique=True)
-    studio = models.CharField(max_length=64, unique=True)
+    studio = models.CharField(max_length=64, unique=False)
     release_date = models.DateField("date released")
